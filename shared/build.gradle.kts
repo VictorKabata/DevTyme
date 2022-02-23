69631c483d9839245d6c5851aecd445cfe4cff86
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-02-23 22:08:58 +0300

    feat: Added ktor dependencies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -19,33 +19,37 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-            }
-        }
 
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
+                implementation(Dependencies.coroutinesKmm)
+
+                implementation(Dependencies.koinCore)
+
+                implementation(Dependencies.kotlinxSerialization)
+
+                implementation(Dependencies.ktorCore)
+                implementation(Dependencies.ktorSerialization)
+                implementation(Dependencies.ktorLogging)
+
+                // implementation(Dependencies.realm)
             }
         }
 
         val androidMain by getting {
             dependencies {
+                implementation(Dependencies.ktorAndroid)
             }
         }
 
-        val androidTest by getting
-
         val iosX64Main by getting
-
         val iosArm64Main by getting
-
-        val iosSimulatorArm64Main by getting
-
         val iosMain by creating {
             dependsOn(commonMain)
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
+
+            dependencies {
+                implementation(Dependencies.ktoriOS)
+            }
         }
 
         /*val iosMain by getting {
@@ -53,14 +57,20 @@ kotlin {
             }
         }*/
 
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+
+        val androidTest by getting
+
         val iosX64Test by getting
         val iosArm64Test by getting
-        val iosSimulatorArm64Test by getting
         val iosTest by creating {
             dependsOn(commonTest)
             iosX64Test.dependsOn(this)
             iosArm64Test.dependsOn(this)
-            iosSimulatorArm64Test.dependsOn(this)
         }
     }
 }
