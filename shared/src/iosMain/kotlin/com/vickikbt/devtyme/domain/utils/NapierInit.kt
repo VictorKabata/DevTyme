a14b9c115cc16f46a0cd60ae49f7dcd51a93008b
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-02-28 18:32:31 +0300

    feat: Created napier instance for android and ios platforms

---
+++ shared/src/iosMain/kotlin/com/vickikbt/devtyme/domain/utils/NapierInit.kt
@@ -0,0 +1,10 @@
+package com.vickikbt.devtyme.domain.utils
+
+import io.github.aakira.napier.DebugAntilog
+import io.github.aakira.napier.Napier
+
+actual class NapierInit actual constructor() {
+    actual fun init() {
+        Napier.base(DebugAntilog())
+    }
+}
