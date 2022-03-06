d1f99b041c7aa729275893b63e30057e7e772c4e
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-03-06 03:49:01 +0300

    feat: Setup app theme

--- androidApp/src/main/java/com/vickikbt/devtyme/android/ui/components/DateTabs.kt
+++ androidApp/src/main/java/com/vickikbt/devtyme/android/ui/components/DateTabs.kt
@@ -1,5 +1,6 @@
 package com.vickikbt.devtyme.android.ui.components
 
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Tab
 import androidx.compose.material.TabRow
@@ -7,9 +8,11 @@ import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -24,13 +27,19 @@ fun DatesTabs(
         modifier = modifier,
         selectedTabIndex = selectedTab ?: 0,
         backgroundColor = MaterialTheme.colors.primary,
-        indicator = { ItemTabIndicator(modifier = Modifier.tabIndicatorOffset(it[selectedTab ?: 0])) }
+        indicator = {
+            ItemTabIndicator(
+                modifier = Modifier.tabIndicatorOffset(
+                    it[selectedTab ?: 0]
+                )
+            )
+        }
     ) {
         dates.forEachIndexed { index, tabItem ->
             val isSelected = selectedTab == index
 
             Tab(
-                // modifier = Modifier.padding(vertical = 4.dp),
+                modifier = Modifier.clip(RoundedCornerShape(10.dp)),
                 selected = selectedTab == index,
                 onClick = { onTabItemClick(index) },
                 text = {
--- androidApp/src/main/java/com/vickikbt/devtyme/android/ui/components/ItemTabIndicator.kt
+++ androidApp/src/main/java/com/vickikbt/devtyme/android/ui/components/ItemTabIndicator.kt
@@ -1,9 +1,7 @@
 package com.vickikbt.devtyme.android.ui.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -14,11 +12,12 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun ItemTabIndicator(modifier: Modifier = Modifier) {
 
-    Box(
-        modifier = modifier
-            .height(52.dp)
-            .clip(RoundedCornerShape(10.dp))
-            .background(MaterialTheme.colors.secondary.copy(alpha = .4f))
-            .padding(vertical = 6.dp)
-    )
+    Column(modifier = Modifier.height(56.dp), verticalArrangement = Arrangement.Center) {
+        Box(
+            modifier = modifier
+                .height(50.dp)
+                .clip(RoundedCornerShape(10.dp))
+                .background(MaterialTheme.colors.secondary.copy(alpha = .4f))
+        )
+    }
 }
