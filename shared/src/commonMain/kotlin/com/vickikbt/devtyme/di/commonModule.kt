409562903e9d6a8d97028f32bd7d3f0af1ac416a
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-03-07 20:28:59 +0300

    feat: Added token interceptor

--- shared/src/commonMain/kotlin/com/vickikbt/devtyme/di/commonModule.kt
+++ shared/src/commonMain/kotlin/com/vickikbt/devtyme/di/commonModule.kt
@@ -3,11 +3,9 @@ package com.vickikbt.devtyme.di
 import com.vickikbt.devtyme.data.cache.realm.AccessTokenDao
 import com.vickikbt.devtyme.data.cache.realm.models.AccessTokenEntity
 import com.vickikbt.devtyme.data.data_sources.AuthRepositoryImpl
-import com.vickikbt.devtyme.data.mappers.toDomain
 import com.vickikbt.devtyme.data.network.ApiService
 import com.vickikbt.devtyme.data.network.ApiServiceImpl
 import com.vickikbt.devtyme.data.network.utils.TokenInterceptor
-import com.vickikbt.devtyme.domain.models.AccessToken
 import com.vickikbt.devtyme.domain.repositories.AuthRepository
 import io.github.aakira.napier.Napier
 import io.ktor.client.*
@@ -19,10 +17,6 @@ import io.ktor.client.request.*
 import io.realm.Configuration
 import io.realm.Realm
 import io.realm.RealmConfiguration
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.launch
 import org.koin.dsl.module
 
 val commonModule = module {
@@ -32,11 +26,9 @@ val commonModule = module {
      * API client via constructor injection
      */
     single {
-        val token = provideToken(accessTokenDao = get())
-
         HttpClient {
             defaultRequest {
-                header("Authorization", "Bearer ${token?.accessToken}")
+                header("Authorization", TokenInterceptor().invoke())
             }
 
             install(Logging) {
@@ -59,7 +51,6 @@ val commonModule = module {
         }
     }
     single<ApiService> { ApiServiceImpl(httpClient = get()) }
-    single { TokenInterceptor(accessTokenDao = get()) }
 
     /**
      *Create instance of realm config need to
@@ -78,13 +69,4 @@ val commonModule = module {
     single { AccessTokenDao(realm = get()) }
 
     single<AuthRepository> { AuthRepositoryImpl(apiService = get(), accessTokenDao = get()) }
-}
-
-private fun provideToken(accessTokenDao: AccessTokenDao): AccessToken? {
-    var token: AccessToken? = null
-    CoroutineScope(Dispatchers.Default).launch {
-        token = accessTokenDao.getToken.first()?.toDomain()
-    }
-
-    return token
 }
