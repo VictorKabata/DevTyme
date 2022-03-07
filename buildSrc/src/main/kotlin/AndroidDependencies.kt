a058fec5e098c8bd2b17fbe31ff5015440bea952
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-03-07 16:47:06 +0300

    clean-up: Removed unused dependencies

--- buildSrc/src/main/kotlin/AndroidDependencies.kt
+++ buildSrc/src/main/kotlin/AndroidDependencies.kt
@@ -13,22 +13,14 @@ object AndroidDependencies {
     const val composeActivity = "androidx.activity:activity-compose:${Versions.composeActivity}"
     const val composeConstraint =
         "androidx.constraintlayout:constraintlayout-compose:${Versions.composeConstraint}"
-    // const val composeCompiler = "1.5.21"
 
     const val lifeCycleRuntime = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycle}"
-    const val accompanistNavigationAnimation =
-        "com.google.accompanist:accompanist-navigation-animation:${Versions.accompanist}"
+    const val accompanistNavigationAnimation = "com.google.accompanist:accompanist-navigation-animation:${Versions.accompanist}"
     const val koinAndroid = "io.insert-koin:koin-android:${Versions.koin}"
     const val koinCompose = "io.insert-koin:koin-androidx-compose:${Versions.koin}"
     const val navigation = "androidx.navigation:navigation-compose:${Versions.navigation}"
     const val coil = "io.coil-kt:coil-compose:${Versions.coil}"
-    const val palette = "28.0.0"
-    const val timber = "com.jakewharton.timber:timber:${Versions.timber}"
-    const val paging = "3.0.0"
     const val coroutines = "1.5.2"
-    const val firebaseBOM = "29.0.3"
-    const val ratingBar = "1.1.1"
-    const val pagingCompose = "1.0.0-alpha14"
     const val splashScreen = "androidx.core:core-splashscreen:${Versions.splashScreen}"
 
     const val jUnit = "4.13.2"
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,14 +3,12 @@ object Versions {
     const val ktor = "1.6.7"
     const val kotlinSerialization = "1.6.10"
     const val kotlinxSerialization = "1.2.2"
-    const val multiplatformPaging = "0.4.7"
-    const val okhttp = "5.0.0-alpha.2"
     const val coroutinesKmm = "1.5.0-native-mt"
     const val realm = "0.9.0"
     const val napier = "2.4.0"
 
     // KMM test libs
-    const val mockk="1.12.3"
+    const val mockk = "1.12.3"
 
     const val gradle = "7.1.1"
     const val kotlin = "1.6.10"
@@ -30,10 +28,8 @@ object Versions {
     const val koin = "3.1.2"
     const val navigation = "2.4.0-alpha02"
     const val coil = "1.4.0"
-    const val timber = "5.0.1"
     const val coroutines = "1.5.2"
     const val firebaseBOM = "29.0.3"
-    const val ratingBar = "1.1.1"
     const val splashScreen = "1.0.0-beta01"
 
     const val jUnit = "4.13.2"
@@ -41,5 +37,4 @@ object Versions {
     const val coroutinesTest = "1.5.0"
     const val robolectic = "4.5.1"
     const val mockWebServer = "4.7.2"
-
 }
